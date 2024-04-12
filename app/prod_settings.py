aedf3f8b9f7421abf3113a38d1ed214d223fb701
shevchenkokubg <ooshevchenko.fitm23@kubg.edu.ua>
2024-04-12 20:57:09 +0300

    deploy

--- app/prod_settings.py
+++ app/prod_settings.py
@@ -18,7 +18,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'djfofoejfnfnnefem934483948mcnd4ango-inselffeklfenrkkercure-&(z--.0um32m0bbw9-kd7j0hdi44n8_rj%ahf*dv30@&6*t!@kaqly&n$-'
+SECRET_KEY = 'djfofoejfnfnnefem934483948mcnd4ango-inselffeklfenrkkercure-&(z--.0um32m0bbw9-kd7j0hdi44n8_rj%ahf*dv30hlidsvjklslkjsdvnlsvdjl0987397878390737300u....87373iefhfeifhoeofhie@&6*t!@kaqly&n$-'
 
 DEBUG = False  # Встановлення DEBUG на False для продукційного середовища
 
